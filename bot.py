db221a52c3cca9b2820d1d445a9fe7295bf6611d
Samu1718508 <enricoeth@proton.me>
2026-08-21 14:17:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -238,7 +238,8 @@ async def main():
     print("Bot avviato!")
     await app.run_polling(drop_pending_updates=True)
 
-
 if __name__ == "__main__":
     print("STO PER AVVIARE MAIN!")
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
